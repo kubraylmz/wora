74b7b7dc290564bb0602b0240863440491f12d9e
harundemiray <demirayharun94@gmail.com>
2018-03-20 14:20:42 +0300

    ilk commit

---
+++ wora/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wora.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Lokasyon() {
+
+            return View();
+        }
+
+        public ActionResult blog()
+        {
+            return View();
+        }
+    }
+}
